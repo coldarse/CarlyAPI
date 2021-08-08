32c1d63357b984aa9c15f05f3f921f7ec76c91e7
coldarse <37390180+coldarse@users.noreply.github.com>
2021-08-08 23:37:37 +0800

    Added PagedPackageResultRequestDto.cs

---
+++ src/Carly.Application/Packages/Dto/PagedPackageResultRequestDto.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Services.Dto;
+
+namespace Carly.Packages
+{
+    public class PagedPackageResultRequestDto : PagedResultRequestDto
+    {
+        public string Keyword { get; set; }
+    }
+}
